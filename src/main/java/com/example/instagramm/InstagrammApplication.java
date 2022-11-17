31aceff37ecf261dbd77047cbede47ce48037535
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-11-17 20:59:40 +0300

    Начало работы над бэк-часть аналога Instagramm

---
+++ src/main/java/com/example/instagramm/InstagrammApplication.java
@@ -0,0 +1,11 @@
+package com.example.instagramm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagrammApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InstagrammApplication.class, args);
+	}
+}
