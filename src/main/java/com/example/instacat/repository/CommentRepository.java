312901cd6b53e55db6d77e7a327b8d14c54feaed
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-11-18 11:42:04 +0300

    Добавлены репозитории для обработки сущностей.

---
+++ src/main/java/com/example/instacat/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.example.instacat.repository;
+
+import com.example.instacat.entity.Comment;
+import com.example.instacat.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPost(Post post);
+
+    Optional<Comment> findByIdAndUserId(Long postId, Long userId);
+}
---
+++ src/main/java/com/example/instacat/repository/PostRepository.java
@@ -0,0 +1,18 @@
+package com.example.instacat.repository;
+
+import com.example.instacat.entity.Post;
+import com.example.instacat.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByCreateDate();
+
+    List<Post> findAllByUserOrderByCreateDateDesc(User user);
+
+    Optional<Post> findPostByIdAndUser(Long id, User user);
+}
