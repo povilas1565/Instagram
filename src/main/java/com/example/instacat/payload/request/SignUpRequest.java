6b5ca46be9341e87083848a0a951705113e6e1c5
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-11-19 13:45:22 +0300

    Добавлены запросы полезной нагрузки.

---
+++ src/main/java/com/example/instacat/payload/request/SignUpRequest.java
@@ -0,0 +1,32 @@
+package com.example.instacat.payload.request;
+import com.example.instacat.annotations.ValidEmail;
+import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+public class SignUpRequest {
+    @Email(message = "It should be email format")
+    @NotBlank(message = "User email is required")
+    @ValidEmail
+    private String email;
+
+    @NotEmpty(message = "Please enter your name")
+    private String firstname;
+
+    @NotEmpty(message = "Please enter your lastname")
+    private String lastname;
+
+    @NotEmpty(message = "Please enter your username")
+    private String username;
+
+    @NotEmpty(message = "Please enter your password")
+    @Size(min = 5, max = 50)
+    private String password;
+
+    @NotEmpty(message = "Please enter your password again")
+    @Size(min = 5, max = 50)
+    private String confirmPassword;
+}
